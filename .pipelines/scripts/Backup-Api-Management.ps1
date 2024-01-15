f3fe6edd516eaf69aa79b324837f8a9a3e589f40
Chris House <chhouse@microsoft.com>
2024-01-15 12:46:36 -0600

    added script

--- .pipelines/scripts/Backup-Api-Management.ps1
+++ .pipelines/scripts/Backup-Api-Management.ps1
@@ -1,1 +1,26 @@
-c:\Users\chhouse\Downloads\dr-api-san.pfx
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ApiManagementName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ApiManagementResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $StorageAccountName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $StorageResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ContainerName
+)
+
+$blobName = "$ApiManagementName-$(Get-Date -Format 'yyyy-MM-dd-HH-mm-ss').bak"
+
+$storageKey = (Get-AzStorageAccountKey -ResourceGroupName $StorageResourceGroup -StorageAccountName $StorageAccountName)[0].Value
+
+$storageContext = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storageKey
+
+Backup-AzApiManagement -ResourceGroupName $ApiManagementResourceGroup -Name $ApiManagementName `
+    -StorageContext $storageContext -TargetContainerName $ContainerName -TargetBlobName $blobName
